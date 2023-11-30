085fb52610c3000c4c922625b7911ec617ec04c6
dreamagici4n <lucascostamr812@gmail.com>
2023-11-30 14:02:48 -0300

    chore: implementado docker compose

--- coup-online-master/server/model/login.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    email varchar(80),
-    password varchar(255),
-    nickname varchar(20)
-);
-
-INSERT INTO users(email, password, nickname) VALUES('email@email', '1234', 'nickname');
