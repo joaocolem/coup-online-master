96a79c92dbcde1b0de2f3b693c47de1fb600abd9
JoaoColem <joaocostaval@gmail.com>
2024-01-03 13:55:59 -0300

    fix event listener

--- coup-online-master/server/gameRefactor/EventListener.js
+++ coup-online-master/server/gameRefactor/EventListener.js
@@ -1,56 +1,50 @@
-const {
-    BlockChallengeDecisionHandler,
-  } = require('./gameHandlers/challengers/blockChallengeDecisionHandler');
+const 
+    BlockChallengeDecisionHandler= require('./gameHandlers/challengers/blockChallengeDecisionHandler');
 
-  const {
-    BlockDecisionHandler,
-  } = require('./gameHandlers/challengers/blockDecisionHandler');
+  const 
+    BlockDecisionHandler = require('./gameHandlers/challengers/blockDecisionHandler');
 
-  const {
-    ChallengeDecisionHandler,
-  } = require('./gameHandlers/challengers/challengeDecisionHandler');
+  const 
+    ChallengeDecisionHandler = require('./gameHandlers/challengers/challengeDecisionHandler');
 
-  const {
-    ChooseExchangeDecisionHandler,
-  } = require('./gameHandlers/chooseCards/chooseExchangeDecisionHandler');
+  const 
+    ChooseExchangeDecisionHandler = require('./gameHandlers/chooseCards/chooseExchangeDecisionHandler');
   
-  const {
-    ChooseInfluenceDecisionHandler,
-  } = require('./gameHandlers/chooseCards/chooseInfluenceDecisionHandler');
+  const 
+    ChooseInfluenceDecisionHandler = require('./gameHandlers/chooseCards/chooseInfluenceDecisionHandler');
   
-  const {
-    PlayAgainHandler,
-  } = require('./gameHandlers/game/playAgainHandler');
+  const 
+    PlayAgainHandler= require('./gameHandlers/game/playAgainHandler');
+
+  const 
+    RevealDecisionHandler= require('./gameHandlers/game/revealDecisionHandler');
   
-  const {
-    RevealDecisionHandler,
-  } = require('./gameHandlers/game/revealDecisionHandler');
+  const CoinHandler =require('./gameHandlers/game/coinHandler');
   
-  const {
-    CoinHandler,
-  } = require('./gameHandlers/game/coinHandler');
-  
-  const {
-    ActionController,
-  } = require('./control/actionController');
-  
+  const ActionController = require('./control/actionController');
 
 class EventListener {
-    static listen(gameSocket, players, match) {
-        players.forEach(player => {
-            const socket = gameSocket.sockets[player.socketID];
+  constructor(gameSocket, players, match) {
+    this.match = match;
+    this.gameSocket = gameSocket;
+    this.players = players;
+}
 
-            socket.on('g-playAgain', () => PlayAgainHandler.handlePlayAgain(match));
-            socket.on('g-deductCoins', (res) => CoinHandler.handleDeductCoins(match, res));
-            socket.on('g-challengeDecision', (res) => ChallengeDecisionHandler.handleChallengeDecision(match, res));
-            socket.on('g-blockChallengeDecision', (res) => BlockChallengeDecisionHandler.handleBlockChallengeDecision(match, res));
-            socket.on('g-blockDecision', (res) => BlockDecisionHandler.handleBlockDecision(match, res));
-            socket.on('g-revealDecision', (res) => RevealDecisionHandler.handleRevealDecision(match, res));
-            socket.on('g-chooseInfluenceDecision', (res) => ChooseInfluenceDecisionHandler.handleChooseInfluenceDecision(match, res));
-            socket.on('g-chooseExchangeDecision', (res) => ChooseExchangeDecisionHandler.handleChooseExchangeDecision(match, res));
-            socket.on('g-blockAssassinateProblem', (res) => BlockAssassinateProblemHandler.handleBlockAssassinateProblem(match, res));
-            socket.on('g-actionDecision', (res) => ActionController.handleActionDecision(match, res));
-        });
+     listen() {
+        this.players.forEach(player => {
+            const socket = this.gameSocket.sockets[player.socketID];
+
+            socket.on('g-playAgain', () => PlayAgainHandler.handlePlayAgain(this.match));
+            socket.on('g-deductCoins', (res) => CoinHandler.handleDeductCoins(this.match, res));
+            socket.on('g-challengeDecision', (res) => ChallengeDecisionHandler.handleChallengeDecision(this.match, res));
+            socket.on('g-blockChallengeDecision', (res) => BlockChallengeDecisionHandler.handleBlockChallengeDecision(this.match, res));
+            socket.on('g-blockDecision', (res) => BlockDecisionHandler.handleBlockDecision(this.match, res));
+            socket.on('g-revealDecision', (res) => RevealDecisionHandler.handleRevealDecision(this.match, res));
+            socket.on('g-chooseInfluenceDecision', (res) => ChooseInfluenceDecisionHandler.handleChooseInfluenceDecision(this.match, res));
+            socket.on('g-chooseExchangeDecision', (res) => ChooseExchangeDecisionHandler.handleChooseExchangeDecision(this.match, res));
+            socket.on('g-blockAssassinateProblem', (res) => BlockAssassinateProblemHandler.handleBlockAssassinateProblem(this.match, res));
+            socket.on('g-actionDecision', (res) => ActionController.handleActionDecision(this.match, res));
+                  });
     }
 }
 
--- coup-online-master/server/gameRefactor/control/actionController.js
+++ coup-online-master/server/gameRefactor/control/actionController.js
@@ -1,36 +1,42 @@
-const {
-    IncomeHandler,
-  } = require('../gameHandlers/actions/incomeHandler');
+const 
+    IncomeHandler = require('../gameHandlers/actions/incomeHandler');
   
-  const {
-    ForeignAidHandler,
-  } = require('../gameHandlers/actions/foreingAindHandler');
+  const 
+    ForeignAidHandler= require('../gameHandlers/actions/foreingAindHandler');
   
-  const {
-    CoupHandler,
-  } = require('../gameHandlers/actions/coupHandler');
+  const 
+    CoupHandler = require('../gameHandlers/actions/coupHandler');
   
-  const {
-    TaxHandler,
-  } = require('../gameHandlers/actions/taxHandler');
+  const 
+    TaxHandler= require('../gameHandlers/actions/taxHandler');
   
-  const {
-    AssassinateHandler,
-  } = require('../gameHandlers/actions/assassinateHandler');
+  const 
+    AssassinateHandler = require('../gameHandlers/actions/assassinateHandler');
   
-  const {
-    ExchangeHandler,
-  } = require('../gameHandlers/actions/exchangeHandler');
+  const 
+    ExchangeHandler= require('../gameHandlers/actions/exchangeHandler');
   
-  const {
-    StealHandler,
-  } = require('../gameHandlers/actions/stealHandler');
+  const 
+    StealHandler= require('../gameHandlers/actions/stealHandler');
   
   
 
 class ActionController {
+    static handleActionDecision(match, res) {
+       
+        const { action } = res;
 
-    
+        if (match.actions && match.actions[action.action] && match.actions[action.action].isChallengeable) {
+            match.openChallenge(action, match.actions[action.action].blockableBy.length > 0, match);
+        } else if (action.action === 'foreign_aid') {
+            match.isChallengeBlockOpen = true;
+            match.gameSocket.emit("g-openBlock", action);
+        } else {
+            this.#applyAction(action, match); 
+        }
+        
+    }
+
     static #applyAction(action, match) {
         let logTarget = '';
 
@@ -70,19 +76,6 @@ class ActionController {
         }
     }
 
-    static handleActionDecision(res, match) {
-       
-        const { action } = res;
-
-        if (match.actions[action.action].isChallengeable) {
-            match.openChallenge(action, match.actions[action.action].blockableBy.length > 0, match);
-        } else if (action.action === 'foreign_aid') {
-            match.isChallengeBlockOpen = true;
-            match.gameSocket.emit("g-openBlock", action);
-        } else {
-            this.#applyAction(action, match); 
-        }
-    }
 }
 
 module.exports = ActionController;
